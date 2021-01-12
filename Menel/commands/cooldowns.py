2c4eb3e99db221ff29e6d8987ca2f6f8b5da2a11
JelNiSław <s.jelnicki@gmail.com>
2021-01-12 18:48:41 +0100

    Add cooldowns command

---
+++ Menel/commands/cooldowns.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from math import ceil
+
+from discord import Colour, Embed
+
+from functions.plural_time import plural_time
+from objects.cooldowns import cooldowns
+
+
+def setup(cliffs):
+    @cliffs.command('cooldowns', name='cooldowns', cooldown=2)
+    async def command(m):
+        if not cooldowns.cooldowns[m.author.id]:
+            await m.send('Nie masz żadnych spowolnień.')
+            return
+
+        cooldown_list = set()
+        for name, time in list(cooldowns.cooldowns[m.author.id].items())[:10]:
+            cooldown_list.add(f'{name}: {plural_time(ceil(time - datetime.utcnow().timestamp()))}')
+
+        await m.send(embed=Embed(
+            title='Twoje obecne spowolnienia',
+            description='\n'.join(cooldown_list),
+            colour=Colour.blue())
+        )
