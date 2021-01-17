6fcefc207500216c2bca5e255a29b8081d55ed19
JelNiSław <s.jelnicki@gmail.com>
2021-01-17 22:26:13 +0100

    Improve auto import

--- Menel/setup/import_commands.py
+++ Menel/setup/import_commands.py
@@ -6,7 +6,13 @@ from cliffs import CommandDispatcher
 from Menel import commands
 
 
+COMMANDS = dict()
+
+
 # Inspired by https://github.com/python-discord/bot/blob/master/bot/utils/extensions.py
+
+def unqualify(name: str) -> str:
+    return name.rsplit('.', maxsplit=1)[-1]
 
 
 def import_commands(cliffs: CommandDispatcher):
@@ -14,15 +20,16 @@ def import_commands(cliffs: CommandDispatcher):
     skipped = set()
 
     for module in pkgutil.walk_packages(commands.__path__, commands.__name__ + '.'):
-        if module.name.rsplit('.', maxsplit=1)[-1].startswith('_'):
+        if unqualify(module.name).startswith('_'):
             continue
 
         module = importlib.import_module(module.name)
+        name = unqualify(module.__name__)
         if hasattr(module, 'setup'):
             module.setup(cliffs)
-            imported.add(module.__name__)
+            imported.add(name)
         else:
-            skipped.add(module.__name__)
+            skipped.add(name)
 
     print(f'Imported {", ".join(imported)}')
     if skipped:
