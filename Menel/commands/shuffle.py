cf622aed00fa170e9138dab9d76b1c7c6fe493cd
jel <25802745+jelni@users.noreply.github.com>
2021-01-18 15:52:38 +0100

    Add shuffle command

---
+++ Menel/commands/shuffle.py
@@ -0,0 +1,11 @@
+import random
+
+from ..objects.message import Message
+
+
+def setup(cliffs):
+    @cliffs.command('(shuffle|shuffle-text) <text...>', name='shuffle', cooldown=2)
+    async def command(m: Message, text):
+        text = text.split()
+        random.shuffle(text)
+        await m.send(' '.join(text))
