4fe42e72a0212634e208d80f14e71fc53f8f6ff3
jel <25802745+jelni@users.noreply.github.com>
2021-02-09 14:21:40 +0100

    Improve ping command

--- Menel/commands/bot/ping.py
+++ Menel/commands/bot/ping.py
@@ -1,6 +1,8 @@
+from time import perf_counter
+
 import discord
 
-from ...objects import Category, Command, Message
+from ...objects import bot, Category, Command, Message
 
 
 COMMAND = Command(
@@ -15,13 +17,19 @@ COMMAND = Command(
 def setup(cliffs):
     @cliffs.command('ping', command=COMMAND)
     async def command(m: Message):
+        start = perf_counter()
         message = await m.send('Pong!')
+        stop = perf_counter()
 
-        delay = round((message.created_at.timestamp() - m.created_at.timestamp()) * 1000)
-
-        result = f'Ping: {delay} ms'
+        embed = discord.Embed(
+            description=f'Ogólne opóźnienie wiadomości: '
+                        f'{round((message.created_at.timestamp() - m.created_at.timestamp()) * 1000)} ms\n'
+                        f'Czas wysyłania wiadomości: {round((stop - start) * 1000)} ms\n'
+                        f'Opóźnienie WebSocket: {round(bot.latency * 1000)} ms',
+            colour=discord.Colour.blurple()
+        )
 
         try:
-            await message.edit(content=result)
+            await message.edit(content=None, embed=embed)
         except discord.HTTPException:
-            await m.send(result)
+            pass
