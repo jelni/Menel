930546fdf60f47624248dcda0051edabd0a7a7f3
jel <25802745+jelni@users.noreply.github.com>
2021-03-29 14:17:24 +0200

    Allow attachments in pxseu command

--- Menel/__main__.py
+++ Menel/__main__.py
@@ -1,15 +1,12 @@
-from os import getenv
-
-from dotenv import load_dotenv
-
-
-load_dotenv(override=True)
-
-
 def main():
-    from . import commands, handlers
-    from .objects import bot
+    from dotenv import load_dotenv
+
+    load_dotenv(override=True)
+
+    from os import getenv
     from .setup import auto_import, cliffs
+    from .objects import bot
+    from . import commands, handlers
 
     auto_import(commands, cliffs, add_to_help=True)
     auto_import(handlers, bot, add_to_help=False)
--- Menel/commands/other/pxseu.py
+++ Menel/commands/other/pxseu.py
@@ -9,22 +9,36 @@ from ...objects import Category, Command, Message
 COMMAND = Command(
     'pxseu',
     syntax=None,
-    description='Wysyła anonimową wiadomość przez API pxseu.',
+    description='Wysyła wiadomość przez API pxseu.',
     category=Category.OTHER,
     cooldown=5
 )
 
 
 def setup(cliffs):
-    @cliffs.command('pxseu [(name|nick) <name>] <message...>', command=COMMAND)
-    async def command(m: Message, message, name=None):
+    @cliffs.command('pxseu {[(name|nick) <name>][(attachment|att|image|img) <url>]} <message...>', command=COMMAND)
+    async def command(m: Message, message, name=None, url=None):
+        if url:
+            attachment = url
+        elif m.attachments:
+            attachment = m.attachments[0].url
+        else:
+            attachment = None
+
         async with aiohttp.request(
                 'POST', 'https://www.pxseu.com/api/v2/sendMessage',
-                json={'name': name, 'message': message, 'user': m.author.id},
+                json={
+                    'message': message,
+                    'name': name,
+                    'attachment': attachment,
+                    'user': m.author.id
+                },
                 headers={'Authorization': 'Bearer ' + getenv('PXSEU_MESSAGE_TOKEN')},
                 timeout=aiohttp.ClientTimeout(total=10)
         ) as r:
-            if r.status == 200:
-                await m.success('good')
-            else:
-                await m.error(clean_content((await r.json())['message']))
+            json = await r.json()
+
+        if json['status'] == 200:
+            await m.success('good')
+        else:
+            await m.error(f'{json["status"]}: {clean_content(json["message"])}')
